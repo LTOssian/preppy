9156cb885d44ad5aa57128237cc3d8688e64fc35
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-06-02 23:47:50 +0200

    feat: initiate routing for home & not found pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "**",
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HeroBannerComponent } from './home/hero-banner/hero-banner.component';
+import { ReinsuranceElementComponent } from './home/reinsurance-element/reinsurance-element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    NotFoundComponent,
+    HeroBannerComponent,
+    ReinsuranceElementComponent
   ],
   imports: [
     BrowserModule,
