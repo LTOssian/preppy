e56dba70e3b9a7cacd0d7e35e117e97a0427ac07
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-06-03 00:41:30 +0200

    feat(test): unit tests for the hero banner

--- src/app/home/hero-banner/hero-banner.component.spec.ts
+++ src/app/home/hero-banner/hero-banner.component.spec.ts
@@ -18,4 +18,24 @@ describe('HeroBannerComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the hero title', () => {
+    const titleElement: HTMLHeadingElement = fixture.nativeElement.querySelector('h1');
+    expect(titleElement.textContent).toContain(component.heroTitle);
+  });
+
+  it('should display the hero tags', () => {
+    const tagElements: HTMLSpanElement[] = fixture.nativeElement.querySelectorAll('.heroTag');
+    expect(tagElements.length).toBe(component.heroTags.length);
+
+    Array.from(tagElements).forEach((tagElement, i: number) => {
+      expect(tagElement.textContent).toContain(component.heroTags[i])
+    });
+  });
+
+  it('should display the hero subTitle', () => {
+    const subTitleElement: HTMLHeadingElement = fixture.nativeElement.querySelector('h2');
+    expect(subTitleElement.textContent).toContain(component.heroSubTitle);
+  })
+
 });
